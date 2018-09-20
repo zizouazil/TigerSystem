dcabb3d9a70ff9e3671766c54cf77a8d2a885cda
zizouazil <40892963+zizouazil@users.noreply.github.com>
2018-09-20 16:19:20 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -483,7 +483,28 @@ if(command === `تكلم`) {
 
 }); 
  
+client.on('message', message => {
+	var prefix = "..";
+ if(message.content.startsWith(prefix + "ادخل")) {
+message.member.voiceChannel.join();
+}
+});
 
+==============================
+client.on('message', msg => {
+
+    if (msg.content == '1join') {
+        if (msg.member.voiceChannel) {
+
+     if (msg.member.voiceChannel.joinable) {
+         msg.member.voiceChannel.join().then(msg.react('✅'));
+     }
+    }
+}
+})
+client.on('ready', () => {
+    client.channels.get("492338887944044576").join();
+    });
 
 
   client.login(process.env.BOT_TOKEN);
