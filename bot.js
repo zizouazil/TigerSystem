4fdbe5b37ce9df03e16fe87249b2ba2615052dca
zizouazil <40892963+zizouazil@users.noreply.github.com>
2018-09-20 16:34:09 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -494,4 +494,14 @@ client.on('guildMemberAdd', member => {
  });
 });
 
+client.on('message', message => {
+    if(message.channel.type === 'dm') {
+        var guildID = '492244973228523520'; // <=============== ايدي السيرفر حقك
+        if(message.content.includes('discord.gg/')) {
+            var member = client.guilds.find(g => g.id === guildID).members.find(m => m.id === message.author.id);
+            member.ban({ reason: 'ADS In Private.' }).catch();
+        }
+    }
+});
+
   client.login(process.env.BOT_TOKEN);
