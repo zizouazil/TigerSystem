c7bff838cb2443aaa696bde88ce2b1f5c7de6015
zizouazil <40892963+zizouazil@users.noreply.github.com>
2018-09-21 14:09:57 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -324,7 +324,7 @@ var prefix = "..";
    『..ترحيب  ❖ 』
    『..clear   ❖ مسح 100 رسالة』
    『..botinfo ❖ اعدادت البوت』
-   『..invites 』
+   『..myinv ❖ الدعوات حقك 』
    『..id  ❖ 』
    『..server  ❖  』
    『..صورة السرفر 』
@@ -753,6 +753,55 @@ client.on('message', function(message) {
             .setFooter(`من (@${message.author.tag})  |  (${message.author.id})`)
         client.channels.get("492667082962370570").send({ embed: stewart });
     }
+});
+
+client.on('message',message =>{
+      var prefix = "..";
+ if(message.content.split(' ')[0].toLowerCase() == prefix + 'myinv') {
+let guild = message.guild
+var codes = [""]
+ var nul = 0
+      
+guild.fetchInvites()
+.then(invites => {
+invites.forEach(invite => {
+if (invite.inviter === message.author) {
+    nul+=invite.uses
+codes.push(`discord.gg/${invite.code}`)
+}
+ 
+})
+  if (nul > 0) {
+      const e = new Discord.RichEmbed()
+      .addField(`${message.author.username}`, `لقد قمت بدعوة **${nul}** شخص`)
+      .setColor('#36393e')
+      message.channel.send(e)
+  }else {
+                       var embed = new Discord.RichEmbed()
+                        .setColor("#000000")
+                        .addField(`${message.author.username}`, `لم تقم بدعوة أي شخص لهذة السيرفر`)
+
+                       message.channel.send({ embed: embed });
+                        return;
+                    }
+}).then(m => {
+if (codes.length < 0) {
+    var embed = new Discord.RichEmbed()
+.setColor("#000000")
+.addField(`Your invites in ${message.guild.name}`, `You currently don't have any active invites! Please create an invite and start inviting, then you will be able to see your codes here!`)
+message.channel.send({ embed: embed });
+return;
+} else {
+    var embed = new Discord.RichEmbed()
+.setColor("#000000")
+.addField(`Your invites in ${message.guild.name}`, `Invites :\n${codes.join("\n")}`)
+.setColor('#36393e')
+message.channel.send({ embed: embed });
+return;
+}
+})
+}
+
 });
 
 
