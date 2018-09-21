181540ceefbf93de2ba1421178b2e7b344ea63d7
zizouazil <40892963+zizouazil@users.noreply.github.com>
2018-09-21 11:43:10 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -25,7 +25,7 @@ client.user.setGame(`TigerSystem | By Mr.SOKA | `,'https://www.twitch.tv/v5bz');
 client.on('message', message => {
 	var prefix = ".."
     if(!message.channel.guild) return;
-if(message.content.startsWith(prefix +'..bc')) {
+if(message.content.startsWith(prefix +'bc')) {
 if(!message.channel.guild) return message.channel.send('**هذا الأمر فقط للسيرفرات**').then(m => m.delete(5000));
 if(!message.member.hasPermission('ADMINISTRATOR')) return      message.channel.send('**للأسف لا تمتلك صلاحية** `ADMINISTRATOR`' );
 let args = message.content.split(" ").join(" ").slice(2 + prefix.length);
