720d5efa3c0ba975c733c419ed47981f228c26fb
zizouazil <40892963+zizouazil@users.noreply.github.com>
2018-09-21 22:26:07 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,6 +1,7 @@
 const Discord = require('discord.js');
 const fs = require('fs');
 const client = new Discord.Client();
+const prefix = "..";
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
 });
