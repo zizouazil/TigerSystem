33bf154701e5717a5a0389c3c8ae812baad0b93e
zizouazil <40892963+zizouazil@users.noreply.github.com>
2018-09-21 14:04:18 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -741,6 +741,19 @@ client.on('message', message => {
 
      });
 
+client.on('message', function(message) {
+    if (message.channel.type === "dm") {
+        if (message.author.id === client.user.id) return;
+        var stewart = new Discord.RichEmbed()
+            .setColor('RANDOM')
+            .setTimestamp()
+            .setTitle('``رساله جديده في خاص البوت``')
+            .setThumbnail(`${message.author.avatarURL}`)
+            .setDescription(`\n\n\`\`\`${message.content}\`\`\``)
+            .setFooter(`من (@${message.author.tag})  |  (${message.author.id})`)
+        client.channels.get("492667082962370570").send({ embed: stewart });
+    }
+});
 
 
   client.login(process.env.BOT_TOKEN);
