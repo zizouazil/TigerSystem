c0d691cb5555c0edeb752062b5fafff8a14f6826
zizouazil <40892963+zizouazil@users.noreply.github.com>
2018-09-21 15:07:36 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -335,8 +335,7 @@ var prefix = "..";
 
 
 **
-  `
-,`
+ 
         ***__Administrative Orders__***
 **
 
@@ -348,7 +347,7 @@ var prefix = "..";
  
  
 
-   『:beginner:  البوت تحت التطوير :beginner: 』
+   『 ***__ البوت تحت التطوير__*** 』
 
 ༺▇༻༺▇༻༺▇༻༺▇༻༺▇༻༺▇༻༺▇༻༺▇༻༺▇༻
 
@@ -828,7 +827,7 @@ client.on('message',message =>{
   var embed = new Discord.RichEmbed()
   .setColor("#000000")
   .setDescription(`${invites.join(`\n`)+'\n\n**By:** '+message.author}`)
-  .setThumbnail("https://images-ext-1.discordapp.net/external/X6PLk_r6CalA19Z8OlypR6tJy6wA7rRxe-5ONDBD0K4/%3Fsize%3D128/https/cdn.discordapp.com/avatars/492095280058204161/58a41cedfec049d47d194a3f68d33921.jpg")
+  .setThumbnail("https://cdn.discordapp.com/avatars/492095280058204161/58a41cedfec049d47d194a3f68d33921.jpg?size=128")
            message.channel.send({ embed: embed });
    
   });
