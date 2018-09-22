c16bc268046b3f7d56b476a418e9efbbd298428e
zizouazil <40892963+zizouazil@users.noreply.github.com>
2018-09-22 13:31:54 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -911,16 +911,7 @@ client.on('message', async msg => {
 };
 });
 
-client.on ("guildMemberAdd", member => {
-  
-   var role = member.guild.roles.find ("name", "✬ TIGER");
-   member.addRole (role);
-  
-})
 
-client.on ("guildMemberRemove", member => {
-   
-})
 
 
 client.on('message', msg => {
@@ -1095,55 +1086,6 @@ message.channel.send(`**✅ ${user.tag} banned from the server ! ✈ **  `)
 
 
 
-client.on("guildMemberAdd", (member) => {
-
-    let channel = member.guild.channels.get("492244973677182978");
-
-    if (!channel) {
-
-        console.log("!the channel id it's not correct");
-
-        return;
-
-    }
-
-    if (member.id == client.user.id) {
-
-        return;
-
-    }
-
-    console.log('-');
-
-    var guild;
-
-    while (!guild)
-
-        guild = client.guilds.get("492244973677182978");
-
-    guild.fetchInvites().then((data) => {
-
-        data.forEach((Invite, key, map) => {
-
-            var Inv = Invite.code;
-
-            if (dat[Inv])
-
-                if (dat[Inv] < Invite.uses) {
-
- channel.send(`تم دعوته بواسطة  ${Invite.inviter} `) ;       
-
- }
-
-            dat[Inv] = Invite.uses;
-
-       
-
-       });
-
-    });
-
-});
 
 
 client.on('message', async message => {
@@ -1435,6 +1377,17 @@ client.on('message', async message => {
   } // حقوق الفا كودز
 });
 
+
+client.on ("guildMemberAdd", member => {
+  
+   var role = member.guild.roles.find ("name", "✬ TIGER");
+   member.addRole (role);
+  
+})
+
+client.on ("guildMemberRemove", member => {
+   
+})
 
 
   client.login(process.env.BOT_TOKEN);
