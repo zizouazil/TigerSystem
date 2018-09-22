1bc79a444b8ddc5d3761fb056c3b414d47f00bc1
zizouazil <40892963+zizouazil@users.noreply.github.com>
2018-09-22 19:56:58 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1352,6 +1352,14 @@ client.on('message', async message => {
 });
 
 
+client.on('message',function(message) {
+    if(!message.channel.guild) return undefined;
+    const swearWords = ["الشتيمه الي تريد منعه","طيز اختك","طيز ","زب","سوة","خنيث","شرمطوه","كلب","كس","حمار","اختك","امك","قحبه","هنيكك","كس امك","كسمك"];
+    if (swearWords.some(word => message.content.includes(word)) ) {
+      message.delete()
+      message.reply("**عيب عيب عيب عليك كبر مخك شوي :angry: **"); 
+    }
+  }); 
 
 
 
