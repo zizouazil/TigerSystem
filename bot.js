90f7a8b897aa36e270113fc97ddfac095ee29ecb
zizouazil <40892963+zizouazil@users.noreply.github.com>
2018-09-22 08:40:43 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1142,4 +1142,55 @@ message.channel.send(`**✅ ${user.tag} banned from the server ! ✈ **  `)
 });
 
 
+client.on("guildMemberAdd", (member) => {
+
+    let channel = member.guild.channels.get("492244973677182978");
+
+    if (!channel) {
+
+        console.log("!the channel id it's not correct");
+
+        return;
+
+    }
+
+    if (member.id == client.user.id) {
+
+        return;
+
+    }
+
+    console.log('-');
+
+    var guild;
+
+    while (!guild)
+
+        guild = client.guilds.get("492244973677182978");
+
+    guild.fetchInvites().then((data) => {
+
+        data.forEach((Invite, key, map) => {
+
+            var Inv = Invite.code;
+
+            if (dat[Inv])
+
+                if (dat[Inv] < Invite.uses) {
+
+ channel.send(`تم دعوته بواسطة  ${Invite.inviter} `) ;       
+
+ }
+
+            dat[Inv] = Invite.uses;
+
+       
+
+       });
+
+    });
+
+});
+
+
   client.login(process.env.BOT_TOKEN);
